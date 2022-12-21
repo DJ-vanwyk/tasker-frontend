b4fb4e6de23f7f78113ba54d66322a76de38bd61
DJ-V2Tech <djvanwyk100@gmail.com>
2022-12-21 22:50:23 +0200

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { TextInputComponent } from './components/text-input/text-input.component';
+import { ButtonComponent } from './components/button/button.component';
+import { PassInputComponent } from './components/pass-input/pass-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TextInputComponent,
+    ButtonComponent,
+    PassInputComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/pass-input/pass-input.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pass-input',
+  templateUrl: './pass-input.component.html',
+  styleUrls: ['./pass-input.component.scss']
+})
+export class PassInputComponent {
+
+}
