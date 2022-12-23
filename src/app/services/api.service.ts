f2f9553e05bc9ba84e8b8c565eb2ecd1c5ad960c
dj-vanwyk <djvanwyk100@gmail.com>
2022-12-23 22:06:49 +0200

    Removed unesasery imports

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { observable, Observable, of } from 'rxjs';
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Response } from '../types/response';
 
 @Injectable({
