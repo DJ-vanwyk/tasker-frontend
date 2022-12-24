b45a53924b17ec1d69df9deb15a0f383a98ea008
dj-vanwyk <djvanwyk100@gmail.com>
2022-12-24 17:19:33 +0200

    Added form validation

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -12,20 +12,44 @@ import { Response } from '../../types/response';
 export class LoginComponent {
   username: string = '';
   password: string = '';
+  passwordError: string = '';
+  usernameError: string = '';
 
   constructor(private authService: AuthService, private router: Router) {}
 
   onClick() {
-    this.authService.login(this.username, this.password).subscribe(
-      (resp) => {
-        if (resp.code === 200) {
-          this.router.navigateByUrl('tasks');
+    // Resets error messages
+    this.usernameError = '';
+    this.passwordError = '';
+    // Checks if username and password are empty
+    if (this.username === '') {
+      this.usernameError = 'Username is required';
+    }
+
+    if (this.password === '') {
+      this.passwordError = 'Password is required';
+    }
+    //Tries to login if username and password are not empty
+    if (this.username !== '' && this.password !== '') {
+      this.authService.login(this.username, this.password).subscribe(
+        // If login is successful, navigate to tasks page
+        (resp) => {
+          if (resp.code === 200) {
+            this.router.navigateByUrl('tasks');
+          }
+        },
+        (error) => {
+          // Shows error message if username or password is incorrect
+          if (
+            error.status === 401 ||
+            error.status === 400 ||
+            error.status === 404
+          ) {
+            this.password = '';
+            this.passwordError = 'Invalid username or password';
+          }
         }
-        console.log(resp);
-      },
-      (error) => {
-        console.log(error.error);
-      }
-    );
+      );
+    }
   }
 }
