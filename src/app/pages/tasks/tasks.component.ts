62aee37d6a12a89a349f0f3ab5088e08c5a7660f
dj-vanwyk <djvanwyk100@gmail.com>
2022-12-24 11:08:57 +0200

    Moved data fetch to OnInit

--- src/app/pages/tasks/tasks.component.ts
+++ src/app/pages/tasks/tasks.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TasksService } from 'src/app/services/tasks.service';
 import { Task } from '../../types/task';
 import { Router } from '@angular/router';
+import { UsersService } from 'src/app/services/users.service';
+import { StatusesService } from 'src/app/services/statuses.service';
+import { User } from 'src/app/types/user';
+import { Status } from 'src/app/types/status';
 
 @Component({
   selector: 'app-tasks',
@@ -10,14 +14,46 @@ import { Router } from '@angular/router';
 })
 export class TasksComponent {
   tasks: Task[] = [];
+  users: User[] = [];
+  statuses: Status[] = [];
 
-  constructor(private tasksService: TasksService, private router: Router) {
-    tasksService.fetchAll().subscribe(
+  constructor(
+    private tasksService: TasksService,
+    private usersService: UsersService,
+    private statusesService: StatusesService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    // Fetch all tasks
+    this.tasksService.fetchAll().subscribe(
       (res) => {
         this.tasks = res.data;
       },
       (err) => {
-        if (err.status) {
+        if (err.status === 401) {
+          this.router.navigateByUrl('login');
+        }
+      }
+    );
+    // Fetch all users
+    this.usersService.fetchAll().subscribe(
+      (res) => {
+        this.users = res.data;
+      },
+      (err) => {
+        if (err.status === 401) {
+          this.router.navigateByUrl('login');
+        }
+      }
+    );
+    // Fetch all statuses
+    this.statusesService.fetchAll().subscribe(
+      (res) => {
+        this.statuses = res.data;
+      },
+      (err) => {
+        if (err.status === 401) {
           this.router.navigateByUrl('login');
         }
       }
