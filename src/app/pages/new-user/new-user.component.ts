dd9102438e55ed6db69324b26f4cde978fee861f
dj-vanwyk <djvanwyk100@gmail.com>
2022-12-24 18:51:20 +0200

    Added new user page

--- src/app/pages/new-user/new-user.component.ts
+++ src/app/pages/new-user/new-user.component.ts
@@ -1,10 +1,71 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsersService } from 'src/app/services/users.service';
+import { User } from 'src/app/types/user';
 
 @Component({
   selector: 'app-new-user',
   templateUrl: './new-user.component.html',
-  styleUrls: ['./new-user.component.scss']
+  styleUrls: ['./new-user.component.scss'],
 })
 export class NewUserComponent {
+  // Error messages
+  usernameError: string = '';
+  passwordError: string = '';
+  confirmPasswordError: string = '';
 
+  // New user
+  newUser: User = {
+    username: '',
+    password: '',
+  };
+
+  confirmPassword: string = '';
+
+  constructor(private router: Router, private usersService: UsersService) {}
+
+  onCancel() {
+    this.router.navigateByUrl('users');
+  }
+
+  onCreate() {
+    // Reset error messages
+    this.usernameError = '';
+    this.passwordError = '';
+    this.confirmPasswordError = '';
+    // Validate
+    if (this.newUser.username === '') {
+      this.usernameError = 'Username is required';
+    }
+    if (this.newUser.password === '') {
+      this.passwordError = 'Password is required';
+    }
+    if (this.confirmPassword === '') {
+      this.confirmPasswordError = 'Confirm password is required';
+    }
+
+    if (this.newUser.password !== this.confirmPassword) {
+      this.confirmPasswordError = 'Passwords do not match';
+    }
+
+    console.log('All tests passed');
+    if (
+      this.usernameError === '' &&
+      this.passwordError === '' &&
+      this.confirmPasswordError === '' &&
+      this.newUser.password === this.confirmPassword
+    ) {
+      console.log('All tests passed');
+      this.usersService.create(this.newUser).subscribe(
+        (res) => {
+          this.router.navigateByUrl('users');
+        },
+        (err) => {
+          if (err.status === 401) {
+            this.router.navigateByUrl('login');
+          }
+        }
+      );
+    }
+  }
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
 import { Observable } from 'rxjs';
 import { Response } from '../types/response';
+import { User } from '../types/user';
 
 @Injectable({
   providedIn: 'root',
@@ -11,5 +12,9 @@ export class UsersService {
 
   fetchAll(): Observable<Response> {
     return this.api.get('/users');
+  }
+
+  create(user: User): Observable<Response> {
+    return this.api.post('/users', user);
   }
 }
