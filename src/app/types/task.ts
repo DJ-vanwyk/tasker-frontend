95560d414ae403becade3505f1f9ec1ccbcdcb80
dj-vanwyk <djvanwyk100@gmail.com>
2022-12-24 11:27:14 +0200

    Updated Task type to include both num and str

--- src/app/types/task.ts
+++ src/app/types/task.ts
@@ -1,8 +1,8 @@
 export interface Task {
   task_id?: number;
   description: string;
-  assigned_to: number;
-  status: number;
+  assigned_to: number | string;
+  status: number | string;
   due_date: string;
   created_at?: string;
   updated_at?: string;
