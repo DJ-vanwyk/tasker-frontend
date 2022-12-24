c580381d912208749ea234820a51607da00ad86f
dj-vanwyk <djvanwyk100@gmail.com>
2022-12-24 17:19:58 +0200

    Removed unused import from login component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
-import { Response } from '../../types/response';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
